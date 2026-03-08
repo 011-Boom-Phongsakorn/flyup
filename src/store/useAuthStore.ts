f0e61dbbfbd23d6393b6bc26dbae94ec592efec2
phongsakorntangpok <664259011@webmail.npru.ac.th>
2026-03-08 14:59:55 +0700

    Login Form

--- src/store/useAuthStore.ts
+++ src/store/useAuthStore.ts
@@ -7,17 +7,21 @@ interface AuthStore {
     // checkAuth: () => Promise<void>;
     isCheckingAuth: boolean;
     isRegistering: boolean;
+    isLoggingIn: boolean;
     register: (data: any) => Promise<void>;
+    login: (dayta: any) => Promise<void>;
 }
 
 export const useAuthStore = create<AuthStore>((set) => ({
     authUser: null,
     isCheckingAuth: true,
     isRegistering: false,
+    isLoggingIn: false,
     register: async (data) => {
         set({ isRegistering: true })
         try{
             // await api.post('/users/register', data)
+            console.log(data)
             toast.success('สร้างบัญชีสำเร็จ กรุณายืนยันอีเมล์ก่อนเข้าสู่ระบบ')
         }catch(error: any) {
             const errorMessage = error.response?.data?.error;
@@ -28,6 +32,23 @@ export const useAuthStore = create<AuthStore>((set) => ({
             }
         }finally {
             set({ isRegistering: false })
+        }
+    },
+    login: async (data) => {
+        set({ isLoggingIn: true })
+        try{
+            // await api.post('/users/login', data)
+            console.log(data)
+        }catch(error: any){
+            const errorMessage = error.response?.data?.error;
+
+            if (errorMessage === 'please verify email') {
+                toast.error('กรุณายืนยันอีเมล์ก่อนเข้าสู่ระบบ')
+            } else {
+                toast.error('อีเมล์หรือรหัสผ่านไม่ถูกต้อง')
+            }
+        }finally {
+            set({ isLoggingIn: false })
         }
     }
 }))
