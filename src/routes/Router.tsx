d3f9a9b001be55dd73a27dc5b500dde9367dbe0b
Phongsakorn Tangpok <664259011@webmail.npru.ac.th>
2026-03-29 23:46:43 +0700

    fix: deploy

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -3,13 +3,11 @@ import { Toaster } from 'react-hot-toast';
 import { useAuthStore } from '../store/useAuthStore';
 import { useEffect } from 'react';
 
-
 // Layouts
 import MainLayout from '../layouts/MainLayout';
 
 // Public Pages
 import Home from '../pages/public/Home';
-import MainLayout from '../layouts/MainLayout';
 import Projects from '../pages/public/Projects';
 import Register from '../pages/public/Register';
 import Login from '../pages/public/Login';
@@ -31,38 +29,6 @@ import Step4Agreement from '../components/steps/Step4Agreement';
 import Preview from '../pages/pioneer/Preview';
 
 const Router = () => {
-  return (
-    <>
-        <BrowserRouter>
-            <Routes>
-                <Route element={<MainLayout />}>
-                    <Route path='/' element={<Home />} />
-                    <Route path='/register' element={<Register />} />
-                    <Route path='/login' element={<Login />} />
-                    <Route path='/preview/:projectId' element={<Preview />} />
-                </Route>
-
-                <Route>
-                    <Route element={<PioneerLayout />}>
-                        <Route path='/pioneer/dashboard' element={<Dashboard />} />
-                        <Route path='/pioneer/dashboard/projects' element={<MyProjects />} />
-                    </Route>
-                    <Route element={<MainLayout />}>
-                        <Route path='/project/overview/:projectId' element={<ProjectOverview />} />
-                        <Route path='/project/overview/:projectId/step' element={<ProjectStageLayout />}>
-                            <Route index element={<Navigate to="1" replace />} />
-                            <Route path='1' element={<Step1Basics />} />
-                            <Route path='2' element={<Step2Story />} />
-                            <Route path='3' element={<Step3Milestone />} />
-                            <Route path='4' element={<Step4Agreement />} />
-                        </Route>
-                    </Route>
-                </Route>
-            </Routes>
-            <Toaster position='top-right' />
-        </BrowserRouter>
-    </>
-  )
     const { authUser, checkAuth, isCheckingAuth } = useAuthStore()
 
     useEffect(() => {
@@ -88,6 +54,24 @@ const Router = () => {
                         <Route path='/projects' element={<Projects />} />
                         <Route path='/verify' element={<VerifyEmail />} />
                         <Route path='/projects/:id' element={<ProjectDetail />} />
+                        <Route path='/preview/:projectId' element={<Preview />} />
+                    </Route>
+
+                    <Route>
+                        <Route element={<PioneerLayout />}>
+                            <Route path='/pioneer/dashboard' element={<Dashboard />} />
+                            <Route path='/pioneer/dashboard/projects' element={<MyProjects />} />
+                        </Route>
+                        <Route element={<MainLayout />}>
+                            <Route path='/project/overview/:projectId' element={<ProjectOverview />} />
+                            <Route path='/project/overview/:projectId/step' element={<ProjectStageLayout />}>
+                                <Route index element={<Navigate to="1" replace />} />
+                                <Route path='1' element={<Step1Basics />} />
+                                <Route path='2' element={<Step2Story />} />
+                                <Route path='3' element={<Step3Milestone />} />
+                                <Route path='4' element={<Step4Agreement />} />
+                            </Route>
+                        </Route>
                     </Route>
                 </Routes>
                 <Toaster position='top-right' />
